dd2f6bde0a5badb9e5b03cadec485d43ffb4dd48
Lázár Viktor <lazarv1982@gmail.com>
2016-11-29 15:00:02 +0100

    Support substring, substringof, concat method call expressions

--- build/lib/visitor.js
+++ build/lib/visitor.js
@@ -104,6 +104,46 @@ class PGVisitor extends visitor_1.Visitor {
                 else
                     this.where += ` like '${visitor_1.SQLLiteral.convert(params[1].value, params[1].raw).slice(1, -1)}%'`;
                 break;
+            case "substring":
+                this.where += "SUBSTR(";
+                this.Visit(params[0], context);
+                this.where += ", ";
+                this.Visit(params[1], context);
+                this.where += " + 1";
+                if (params[2]) {
+                    this.where += ", ";
+                    this.Visit(params[2], context);
+                }
+                else {
+                    this.where += ", CHAR_LENGTH(";
+                    this.Visit(params[0], context);
+                    this.where += ")";
+                }
+                this.where += ")";
+                break;
+            case "substringof":
+                this.Visit(params[1], context);
+                if (params[0].value == "Edm.String") {
+                    if (this.options.useParameters) {
+                        let value = odata_v4_literal_1.Literal.convert(params[0].value, params[0].raw);
+                        this.parameters.push(`%${value}%`);
+                        this.where += ` like \$${this.parameters.length}`;
+                    }
+                    else
+                        this.where += ` like '%${visitor_1.SQLLiteral.convert(params[0].value, params[0].raw).slice(1, -1)}%'`;
+                }
+                else {
+                    this.where += " like ";
+                    this.Visit(params[0], context);
+                }
+                break;
+            case "concat":
+                this.where += "(";
+                this.Visit(params[0], context);
+                this.where += " || ";
+                this.Visit(params[1], context);
+                this.where += ")";
+                break;
             case "round":
                 this.where += "ROUND(";
                 this.Visit(params[0], context);
--- src/lib/visitor.ts
+++ src/lib/visitor.ts
@@ -103,6 +103,42 @@ export class PGVisitor extends Visitor{
 					this.where += ` like \$${this.parameters.length}`;
 				}else this.where += ` like '${SQLLiteral.convert(params[1].value, params[1].raw).slice(1, -1)}%'`;
 				break;
+			case "substring":
+				this.where += "SUBSTR(";
+				this.Visit(params[0], context);
+				this.where += ", ";
+				this.Visit(params[1], context);
+				this.where += " + 1";
+				if (params[2]){
+					this.where += ", ";
+					this.Visit(params[2], context);
+				}else{
+					this.where += ", CHAR_LENGTH(";
+					this.Visit(params[0], context);
+					this.where += ")";
+				}
+				this.where += ")";
+				break;
+			case "substringof":
+				this.Visit(params[1], context);
+				if (params[0].value == "Edm.String"){
+					if (this.options.useParameters){
+						let value = Literal.convert(params[0].value, params[0].raw);
+						this.parameters.push(`%${value}%`);
+						this.where += ` like \$${this.parameters.length}`;
+					}else this.where += ` like '%${SQLLiteral.convert(params[0].value, params[0].raw).slice(1, -1)}%'`;
+				}else{
+					this.where += " like ";
+					this.Visit(params[0], context);
+				}
+				break;
+			case "concat":
+				this.where += "(";
+				this.Visit(params[0], context);
+				this.where += " || ";
+				this.Visit(params[1], context);
+				this.where += ")";
+				break;
 			case "round":
 				this.where += "ROUND(";
 				this.Visit(params[0], context);
