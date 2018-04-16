7e23cf50c4417a68e2e4e47fc7336d4610eec572
Andriy <andryuha49@windowslive.com>
2018-04-16 10:14:06 +0300

    Fix filter by null (#1)
    * Added possibility to use filter by null

--- build/example/sql.js
+++ build/example/sql.js
@@ -11,11 +11,12 @@ var __param = (this && this.__param) || function (paramIndex, decorator) {
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments)).next());
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 const pg = require("pg");
 const config = require("config");
 const index_1 = require("../lib/index");
@@ -74,8 +75,7 @@ __decorate([
 ], CountriesController.prototype, "getCountries", null);
 __decorate([
     odata_v4_server_1.odata.GET,
-    __param(0, odata_v4_server_1.odata.key),
-    __param(1, odata_v4_server_1.odata.query)
+    __param(0, odata_v4_server_1.odata.key), __param(1, odata_v4_server_1.odata.query)
 ], CountriesController.prototype, "getCountry", null);
 CountriesController = __decorate([
     odata_v4_server_1.odata.type(Country)
@@ -96,13 +96,11 @@ let CitiesController = class CitiesController extends odata_v4_server_1.ODataCon
 };
 __decorate([
     odata_v4_server_1.odata.GET,
-    __param(0, odata_v4_server_1.odata.stream),
-    __param(1, odata_v4_server_1.odata.query)
+    __param(0, odata_v4_server_1.odata.stream), __param(1, odata_v4_server_1.odata.query)
 ], CitiesController.prototype, "getCities", null);
 __decorate([
     odata_v4_server_1.odata.GET,
-    __param(0, odata_v4_server_1.odata.key),
-    __param(1, odata_v4_server_1.odata.query)
+    __param(0, odata_v4_server_1.odata.key), __param(1, odata_v4_server_1.odata.query)
 ], CitiesController.prototype, "getCity", null);
 CitiesController = __decorate([
     odata_v4_server_1.odata.type(City)
@@ -117,8 +115,7 @@ let CountryLanguagesController = class CountryLanguagesController extends odata_
 };
 __decorate([
     odata_v4_server_1.odata.GET,
-    __param(0, odata_v4_server_1.odata.stream),
-    __param(1, odata_v4_server_1.odata.query)
+    __param(0, odata_v4_server_1.odata.stream), __param(1, odata_v4_server_1.odata.query)
 ], CountryLanguagesController.prototype, "getLanguages", null);
 CountryLanguagesController = __decorate([
     odata_v4_server_1.odata.type(CountryLanguage)
